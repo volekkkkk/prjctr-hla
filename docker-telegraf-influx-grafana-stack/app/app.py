c404efac8d9fa0f4070c353260de3e28168651a6
volek <volek17@protonmail.com>
2024-03-17 13:02:48 +0200

    add dummy mongo request to app

--- docker-telegraf-influx-grafana-stack/app/app.py
+++ docker-telegraf-influx-grafana-stack/app/app.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python3
+import datetime
 import os
+import sys
 
 from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("mongo:27017")
+client = MongoClient("mongo:27017", username="admin", password="admin")
+try:
+    client.admin.command('ismaster')
+except:
+    print("Cant connect to Mongo")
+    sys.exit(1)
 
 @app.route('/')
 def main():
+    db = client.test_db
+    recs = db.records
+
+    record = {
+        "author": "me",
+        "log_time": datetime.datetime.now(tz=datetime.timezone.utc)
+    }
+    rec_id: int
     try:
-        client.admin.command('ismaster')
-    except:
-        return "Server not available"
-    return "Hello from the MongoDB client!\n"
+        rec_id = recs.insert_one(record).inserted_id
+    except Exception as e:
+        print(f"Error: {e}")
+        return "Cannot insert value"
+    return f"New value inserted {rec_id}!\n"
 
 
 if __name__ == "__main__":
