87c20a5dc1277767a059a75126d125257ed50b28
volek <volek17@protonmail.com>
2024-03-24 17:04:13 +0200

    add dummy ES requests to app

--- docker-telegraf-influx-grafana-stack/app/app.py
+++ docker-telegraf-influx-grafana-stack/app/app.py
@@ -3,35 +3,60 @@ import datetime
 import os
 import sys
 
+from elasticsearch import Elasticsearch
 from flask import Flask
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient("mongo:27017", username="admin", password="admin")
-try:
-    client.admin.command('ismaster')
-except:
-    print("Cant connect to Mongo")
-    sys.exit(1)
+mongo_client = MongoClient("mongo:27017", username="admin", password="admin")
+es_client = Elasticsearch(["http://elasticsearch:9200"])
 
-@app.route('/')
-def main():
-    db = client.test_db
+
+def dummy_mongo_write():
+    db = mongo_client.test_db
     recs = db.records
 
     record = {
         "author": "me",
-        "log_time": datetime.datetime.now(tz=datetime.timezone.utc)
+        "log_time": datetime.datetime.now(tz=datetime.timezone.utc),
     }
     rec_id: int
     try:
         rec_id = recs.insert_one(record).inserted_id
     except Exception as e:
-        print(f"Error: {e}")
+        app.logger().error(f"Error: {e}")
         return "Cannot insert value"
-    return f"New value inserted {rec_id}!\n"
+    return f"New Mongo value inserted {rec_id}!\n"
+
+def dummy_es_write():
+    doc = {
+        "author": "you",
+        "timestamp": datetime.datetime.now(tz=datetime.timezone.utc),
+    }
+    resp = es_client.index(index="test-index", id=1, document=doc)
+    app.logger.info(resp["result"])
+
+def dummy_es_read():
+    resp = es_client.get(index="test-index", id=1)
+    app.logger.info(resp["_source"])
+
+    es_client.indices.refresh(index="test-index")
+
+    resp = es_client.search(index="test-index", query={"match_all": {}})
+    app.logger.info("Got {} hits:".format(resp["hits"]["total"]["value"]))
+    for hit in resp["hits"]["hits"]:
+        app.logger.info("{timestamp} {author}".format(**hit["_source"]))
+
+
+
+@app.route("/")
+def main():
+    dummy_mongo_write()
+    dummy_es_write()
+    dummy_es_read()
+    return "New values inserted"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("APP_SERVER_PORT", 9090), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("APP_SERVER_PORT", 9090), debug=True)
---
+++ docker-telegraf-influx-grafana-stack/load-test.sh
@@ -0,0 +1,4 @@
+siege --concurrent=15 --reps=500 http://localhost:8080
+siege --concurrent=45 --reps=500 http://localhost:8080
+siege --concurrent=90 --reps=500 http://localhost:8080
+siege --concurrent=200 --reps=500 http://localhost:8080
